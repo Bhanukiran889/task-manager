851857ba6f8af2857e1e51e233010e07285839d7
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-21 20:04:28 +0530

    import header and start implimenting app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Board from "./components/Board";
 import './App.css'
 
-function App() {
+export default function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
-    <></>
-  )
+    <div className="min-h-screen bg-gray-50 ">
+      <Sidebar open={sidebarOpen} toggle={() => setSidebarOpen(v => !v)} />
+      <div className="flex-1 flex flex-col">
+        <Header toggleSidebar={() => setSidebarOpen(v => !v)} />
+        <main className="p-6">
+          <Board />
+        </main>
+      </div>
+    </div>
+  );
 }
-
-export default App
